b167a9a9432b6e84e5359b6d3128aa0d8499de50
Travis Bennett <t_bennett_a@protonmail.com>
2022-11-08 15:46:50 -0500

    Function Typing example

--- React-typescript/src/App.tsx
+++ React-typescript/src/App.tsx
@@ -1,45 +1,42 @@
 import { useState } from "react";
 import "./App.css";
-import List from "./components/List"
+import List from "./components/List";
 
 // Using a ? after defining a property in an interface signifies that the property is optional
 interface IState {
-  people: {
-    name: string,
-    age: number,
-    url: string, 
-    note?: string
-  }[]
+	people: {
+		name: string;
+		age: number;
+		url: string;
+		note?: string;
+	}[];
 }
 function App() {
+	// IState is passed in as the interface that defines the types for each property from ["people"] passed into useState. An empty array has to be defined in the interface, it cannot just be defined in the useState as is the case with regular JSX.
 
-  // IState is passed in as the interface that defines the types for each property from ["people"] passed into useState. An empty array has to be defined in the interface, it cannot just be defined in the useState as is the case with regular JSX.
-  
-  const [people, setPeople] = useState<IState["people"]>([])
-//   const [people, setPeople] = useState([{
-//     name: "Ayrton Senna",
-//     age: 34,
-//     url: "https://www.ayrtonsenna.com.br",
-//     note: "One of the greatest Formula 1 Drivers of all-time"
+	const [people, setPeople] = useState<IState["people"]>([
+		{
+			name: "Ayrton Senna",
+			age: 34,
+			url: "https://media.gq.com/photos/6250a0781879d34b94c04e7d/master/w_1600%2Cc_limit/GettyImages-741201221.jpg",
+			note: "One of the greatest Formula 1 Drivers of all-time",
+		},
+		{
+			name: "Michael Schumacher",
+			age: 53,
+			url: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/A%C3%A9cio_Neves%2C_Michael_Schumacher_e_Didi_%28Cropped%29.jpg/800px-A%C3%A9cio_Neves%2C_Michael_Schumacher_e_Didi_%28Cropped%29.jpg",
+		},
+	]);
 
-//   },
-//   { 
-//     name: "Michael Schumacher",
-//     age: 53,
-//     url: "https://michael-schumacher.de/en/",
+	// If you type people. , it shows the defined types that can be mapped. If the type is undefined previously, it will throw an error
 
-//   }
-// ])
-
-// If you type people. , it shows the defined types that can be mapped. If the type is undefined previously, it will throw an error 
-
-people.map(person => {
-  person.name
-})
+	people.map((person) => {
+		person.name;
+	});
 	return (
 		<div className="app">
 			<h1>Hall of Fame</h1>
-      <List people={people}/>
+			<List people={people} />
 		</div>
 	);
 }
--- React-typescript/src/components/List.tsx
+++ React-typescript/src/components/List.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
 interface IProps {
-    people: {
-      name: string,
-      age: number,
-      url: string, 
-      note?: string
-    }[]
-  }
+	people: {
+		name: string;
+		age: number;
+		url: string;
+		note?: string;
+	}[];
+}
 
-  // How to pass in an interface while performing traditional prop drilling
+// How to pass in an interface while performing traditional prop drilling
 
 // const List = (props: IProps) =>{
 //     return (
@@ -20,12 +20,28 @@ interface IProps {
 // }
 
 // Passing in an interface with a React Functional Component will
-const List: React.FC<IProps> = () =>{
-    return (
-        <div>
-            I am a list
-        </div>
-    )
-}
+const List: React.FC<IProps> = ({ people }) => {
 
-export default List
+	// It's a good idea to define the element being returned instead of letting Typescript infer what is being returned? How to determine what is being returned? Hover the mouse over the function ( renderList) and it will show the return type (JSX.Element [])-an array
+	const renderList = (): JSX.Element [] => {
+		return people.map((person) => {
+			return (
+				<li className="List">
+					<div className="List-header">
+						<img className="List-img" src={person.url} alt="" />
+						<h2>{person.name}</h2>
+					</div>
+					<p>{person.age} years old</p>
+					<p className="List-note">{person.note}</p>
+				</li>
+			);
+		})
+	};
+	return (
+		<ul>
+			{renderList()}
+		</ul>
+	);
+};
+
+export default List;
