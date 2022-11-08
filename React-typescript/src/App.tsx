a63a22ce07d2346b4d3c0f6e79ce839a10e6a9ee
Travis Bennett <t_bennett_a@protonmail.com>
2022-11-08 13:21:55 -0500

    Initial Commit React Typescript exercise

---
+++ React-typescript/src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./App.css";
+
+// Using a ? after defining a property in an interface signifies that the property is optional
+interface IState {
+  people: {
+    name: string,
+    age: number,
+    url: string, 
+    note?: string
+  }[]
+}
+function App() {
+
+  // IState is passed in as the interface that defines the types for each property from ["people"] passed into useState. An empty array has to be defined in the interface, it cannot just be defined in the useState as is the case with regular JSX.
+  
+  const [people, setPeople] = useState<IState["people"]>([])
+//   const [people, setPeople] = useState([{
+//     name: "Ayrton Senna",
+//     age: 34,
+//     url: "https://www.ayrtonsenna.com.br",
+//     note: "One of the greatest Formula 1 Drivers of all-time"
+
+//   },
+//   { 
+//     name: "Michael Schumacher",
+//     age: 53,
+//     url: "https://michael-schumacher.de/en/",
+
+//   }
+// ])
+
+// If you type people. , it shows the defined types that can be mapped. If the type is undefined previously, it will throw an error 
+
+people.map(person => {
+  person.age 
+})
+	return (
+		<div className="app">
+			<h1>Hall of Fame</h1>
+		</div>
+	);
+}
+
+export default App;
